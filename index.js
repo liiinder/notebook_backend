a7229f8b28e49de5e552d231872d979b46fc29d2
Kristoffer Linder <linderkristoffer@hotmail.com>
2022-04-26 13:36:37 +0200

    added a mongodb database

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const { application } = require('express')
+require('dotenv').config()
+const { application, response } = require('express')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         date: "2022-05-30T17:30:31.098Z",
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: "Browser can execute only Javascript",
+//         date: "2022-05-30T18:39:34.091Z",
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         date: "2022-05-30T19:20:14.298Z",
+//         important: true
+//     }
+// ]
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -38,57 +40,41 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
-
 app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    console.log('req.params.id', req.params.id)
+    Note.findById(req.params.id).then(note => {
         res.json(note)
-    } else {
-        res.status(404).send(`There is no note with id #${id}`).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
     notes = notes.filter(note => note.id !== id)
 
     res.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 app.post('/api/notes', (req, res) => {
     const body = req.body
 
     if (!body.content) {
-        return res.status(400).json({
-            error: 'content missing'
-        })
+        return res.status(400).json({ error: 'content missing' })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-
-    res.json(note)
+    note.save().then(savedNote => {
+        res.json(savedNote)
+    })
 })
 
 app.put('/api/notes/:id', (req, res) => {
